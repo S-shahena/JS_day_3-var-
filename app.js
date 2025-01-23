d8f878eb4594e67fba924725874ef28722362d8a
Mst. Shahena Akter <sa4389310@gmail.com>
2025-01-23 12:35:49 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+console.log('shahena');
+
+let num = 5 + 50;
+console.log(num);
+
+num = 50 + 49;
+console.log(num);
---
+++ script.js
@@ -0,0 +1,92 @@
+// var practice work
+// ......................
+// var msBox =5+4;
+// var msBox= 'shorifa';
+// console.log(msBox);
+
+// console.log(thing);
+// var thing='bangladesh';
+
+// var city ='kolkata';
+// console.log(city);
+
+// var name;
+// console.log(name);
+
+// const practice work
+// ..............................
+
+// const myName ='johan done'
+// console.log(myName);
+
+// const myName ='johan';
+// myName ='sarah';
+// console.log(myName);
+
+// {
+//     const favFood='pizza';
+//     console.log(favFood);
+// }
+// console.log(favFood);
+
+// const city ='new york';
+// console.log(city);
+
+// let practice work 
+// .....................................
+// let myName = "john"
+// myName = "shorifa"
+// console.log(myName);
+
+// console.log(myName);
+// let myName = 'sarah';
+
+// let favFood = 'pizza';
+// console.log(favFood);
+
+// var ax = 'shahena akter';
+// console.log(ax);
+
+// .......................................
+
+var shahena = 'bangladesh';
+shahena = 'indea';
+
+var shahena = 'dhaka';
+console.log(shahena);
+
+
+
+let rojina = 'bandorban';
+rojina= 'chittagong';
+console.log(rojina);
+
+
+
+
+const sujon = 'comilla';
+console.log(sujon);
+
+
+
+// ............ aibabe likhle keno error de (let) ................//
+
+// saju= 'khulna';
+// let saju;
+
+// console.log(saju);
+
+
+
+
+
+
+// ............ aibabe likhle keno error de na (var) ................//
+
+// saju = 'khulna';
+// var saju;
+
+// console.log(saju);
+
+
+
